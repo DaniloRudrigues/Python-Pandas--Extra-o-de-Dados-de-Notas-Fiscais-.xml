52862524d9ec5c085dfd27144fa1fe031e61a5ce
DaniloRudrigues_PC_ARPE <danilo.rudrigues@gmail.com>
2022-06-08 12:33:38 -0300

    rascunho

---
+++ totais_mensais_pdf.py
@@ -0,0 +1,4 @@
+from tabula import read_pdf
+
+df = read_pdf(r"C:\Users\danilo.rudrigues.ARPE\Desktop\ARPE - Home\mergam media - bianual\Totais Mensais\2022\total mensal 05-2022\2022.05_Totais_Mensais_GGas.pdf")
+df
